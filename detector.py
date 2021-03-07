53cf414a5a3add1978d61de5f82f1dc7f210ee23
satmonkey <monkey@upcmail.cz>
2021-03-07 18:38:32 +0100

    Add files via upload

--- detector.py
+++ detector.py
@@ -12,10 +12,7 @@ class DetectorTF2:
 		if gpus:
 			# Restrict TensorFlow to only allocate 1GB of memory on the first GPU
 			try:
-				tf.config.experimental.set_virtual_device_configuration(
-					gpus[0],
-					[tf.config.experimental.VirtualDeviceConfiguration(memory_limit=2048)]
-				)
+				tf.config.experimental.set_virtual_device_configuration(gpus[0],[tf.config.experimental.VirtualDeviceConfiguration(memory_limit=1024)])
 				logical_gpus = tf.config.experimental.list_logical_devices('GPU')
 				print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
 			except RuntimeError as e:
